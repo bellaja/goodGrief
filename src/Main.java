0948c1bbf5e00f3a094277df77e4678a4175e755
bellaja <bellaja185@gmail.com>
2023-05-24 18:40:32 +0300

    0

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SalesManager manager = new SalesManager(new int[] {4,5,6,7,8,9});
+        System.out.println(manager.max());
+
+    }
+}
