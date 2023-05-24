df8426c8cd9f79ba8cf670043bcb8ef7aee7003d
bellaja <bellaja185@gmail.com>
2023-05-24 19:02:32 +0300

    1

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,13 +1,15 @@
 public class SalesManager {
-    protected int[] sales;
+    protected long[] sales;
 
-    public SalesManager(int[] sales) {
+
+    public SalesManager(long[] sales) {
         this.sales = sales;
     }
 
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
+    public long max() {
+        long max = sales[0];
+        for (long sale : sales) {
+
             if (sale > max) {
                 max = sale;
             }
